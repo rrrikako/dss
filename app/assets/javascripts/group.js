4f5c40694fa6293aaec6797d295f92d24be0d988
Rikako Kamata <rrrikako0522@gmail.com>
2019-10-27 21:38:20 +0900

    change sidebar

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -3,6 +3,15 @@ $(document).on('turbolinks:load',function(){
   var backgroundImage = $('.group_show__wrapper__image').val();
   $(".group_show__wrapper").css("background-image","url("+ backgroundImage +")");
 
+  //マウスオーバーでサイドバーの表示
+  $('.side_bar').hover(function(){
+    console.log(this);
+    $(this).stop(true).animate({'margin-right':'240px'}, 500);
+  },
+  function(){
+    $(this).animate({'margin-right':'0px'}, 500);
+  }
+  );
 });
 
 //group_indexとgroup_showの音楽再生
