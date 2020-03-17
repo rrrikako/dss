2b2ed723712d0733edfb889b07868dfba313ebdf
Rikako Kamata <rrrikako0522@gmail.com>
2020-03-17 15:35:44 +0900

    edit for .env

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,8 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   config.storage :fog
   config.fog_provider = 'fog/aws'
-  config.fog_directory  = 'dss-backet'
+  # config.fog_directory  = 'dss-backet'
+  # config.asset_host = 'https://dss-bucket.s3.amazonaws.com'
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
@@ -13,5 +14,11 @@ CarrierWave.configure do |config|
     region: ENV['AWS_DEFAULT_REGION'],
     path_style: true
   }
+
+  case Rails.env
+  when 'production'
+      config.fog_directory  = 'dss-backet'
+      config.asset_host = 'https://dss-bucket.s3.amazonaws.com'
+  end
 
 end
